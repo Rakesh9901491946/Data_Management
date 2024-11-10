d9ea32e692714f3698950f3413b698f2e7f846fe
Rakesh Nagaragatta Jayanna <rnj6787@gmail.com>
2024-11-10 17:20:59 +0000

    final

--- models/example/Data_Tranformation.sql
+++ models/example/Data_Tranformation.sql
@@ -1,21 +1,31 @@
 {{ config(materialized='table') }}
 
 SELECT
-    CAST(ID AS INTEGER) AS ID,
+    SAFE_CAST(NULLIF(CAST(ID AS STRING), '') AS INTEGER) AS ID,
     Username,
     `Profile URL`,
-    Type,
-    CAST(`Site Admin` AS BOOLEAN) AS Site_Admin,
+    `Type`,
+    CASE 
+        WHEN `Site Admin` = '1' THEN TRUE 
+        ELSE FALSE 
+    END AS Site_Admin,
     Name,
     Company,
     Blog,
     Location,
     Email,
     Bio,
-    CAST(`Public Repos` AS INTEGER) AS Public_Repos,
-    CAST(`Public Gists` AS INTEGER) AS Public_Gists,
-    CAST(Followers AS INTEGER) AS Followers,
-    CAST(Following AS INTEGER) AS Following,
-    PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', `Account Created At`) AS Account_Created_At,
-    PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', `Last Updated At`) AS Last_Updated_At
-FROM {{ source('github', 'github_users_data') }}
+    SAFE_CAST(NULLIF(CAST(`Public Repos` AS STRING), '') AS INTEGER) AS Public_Repos,
+    SAFE_CAST(NULLIF(CAST(`Public Gists` AS STRING), '') AS INTEGER) AS Public_Gists,
+    SAFE_CAST(NULLIF(CAST(Followers AS STRING), '') AS INTEGER) AS Followers,
+    SAFE_CAST(NULLIF(CAST(`Following` AS STRING), '') AS INTEGER) AS `Following`,
+    PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', NULLIF(CAST(`Account Created At` AS STRING), "0")) AS Account_Created_At,
+    PARSE_TIMESTAMP('%Y-%m-%dT%H:%M:%SZ', NULLIF(CAST(`Last Updated At` AS STRING), "0")) AS Last_Updated_At
+FROM `team-ayra-441115`.`github`.`github_user_data`
+WHERE SAFE_CAST(NULLIF(CAST(ID AS STRING), '') AS INTEGER) IS NOT NULL
+  AND SAFE_CAST(NULLIF(CAST(`Public Repos` AS STRING), '') AS INTEGER) IS NOT NULL
+  AND SAFE_CAST(NULLIF(CAST(`Public Gists` AS STRING), '') AS INTEGER) IS NOT NULL
+  AND SAFE_CAST(NULLIF(CAST(Followers AS STRING), '') AS INTEGER) IS NOT NULL
+  AND SAFE_CAST(NULLIF(CAST(`Following` AS STRING), '') AS INTEGER) IS NOT NULL
+  AND SAFE_CAST(NULLIF(CAST(`Account Created At` AS STRING), "0") AS STRING) LIKE '%T%:%:%Z'
+  AND SAFE_CAST(NULLIF(CAST(`Last Updated At` AS STRING), "0") AS STRING) LIKE '%T%:%:%Z'
